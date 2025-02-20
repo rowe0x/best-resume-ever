e5dd2cf1a7a63b5cce003b1141345fb7cc420cde
rowe0x <rowe0x@192.168.1.155>
2025-02-20 21:54:53 +0800

    update

--- src/pages/resume.vue
+++ src/pages/resume.vue
@@ -39,7 +39,7 @@ export default Vue.component('resume', {
   background: white;
   position: relative;
   width: 21cm;
-  height: 29.68cm;
+  height: 45.68cm;
   display: block;
   page-break-after: auto;
   overflow: hidden;
